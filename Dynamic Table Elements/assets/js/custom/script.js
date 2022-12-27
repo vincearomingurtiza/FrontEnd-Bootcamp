b84c863f36ca377697241a0f3779a2967f1de0c0
iisaiaaah <vincearomingurtiza@gmail.com>
2022-12-27 15:00:23 +0800

    Final Out for Dynamic Table Elements

--- Dynamic Table Elements/assets/js/custom/script.js
+++ Dynamic Table Elements/assets/js/custom/script.js
@@ -1,28 +1,26 @@
 $(document).ready(function(){
     $("body")
-        .on("change", "#select_row", displayRow) 
-        // .on("change", "#select_column", displayColumn)
+        .on("change", "#select_row", displayRow)    /* create table */
 });
 
+/* DOCU: To create tr and td
+ * Triggered by: .on("change", "#select_row", displayRow)
+ * Last Updated Date: December 27, 2022 
+ * @author: Vince
+ * */
 function displayRow(){
     let selected_row_value  = $("#select_row option:selected").val();
     let selected_column_value  = $("#select_column option:selected").val();
-    $("#table_container").empty()
-    
-    for(let counter = 0; counter < selected_row_value; counter++){
-        let create_row = $("<tr>");
+    let table_container = $("#table_container");
+    table_container.empty();
 
-        create_row.attr("data-value-for-row",counter);
+    for(let row_count = 0; row_count < selected_row_value; row_count++){
+        let create_row = $("<tr>");
         $("#table_container").append(create_row);
-    }
-    
-    let get_data_value_for_row = $("tr").attr("data-value-for-row");
-    
-    for(let column_count = 0; column_count < selected_column_value; column_count++){
         
-        let create_column = $("<td> " + get_data_value_for_row + " "+ "," +" " + column_count + " </td>");
-        $("tr").append(create_column);
+        for(let column_count = 0; column_count < selected_column_value; column_count++){ 
+            let create_column = $("<td> "+ row_count +" "+ ","+" "+ column_count+ " </td>");
+            create_row.append(create_column);
+        }
     }
-    // console.log(get_row_count);
-    console.log(get_data_value_for_row);
 };
